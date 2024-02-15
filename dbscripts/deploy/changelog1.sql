1c3e0bf77784b643534f7bb9a0cca05b7d08c432
Chandramani G <gajbharechandramani@gmail.com>
2024-02-15 23:40:43 +0530

    Create changelog1.sql

---
+++ dbscripts/deploy/changelog1.sql
@@ -0,0 +1,17 @@
+-- liquibase formatted sql
+
+-- changeset Chandramani:1
+use database tesla_db_github;
+
+use schema TESLA_SCHEMA;
+
+use warehouse CHANDRAMANI; 
+
+create or replace table Tesla_data( 
+Date date , 
+open double,
+high double,
+low double,
+close double, 
+adj_close double,
+volume bigint );
