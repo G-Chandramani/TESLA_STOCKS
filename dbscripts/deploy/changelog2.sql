fc857413497492bf8d7bb2ddff6c8397955295e8
Chandramani G <gajbharechandramani@gmail.com>
2024-02-16 00:12:12 +0530

    Update changelog2.sql

--- dbscripts/deploy/changelog2.sql
+++ dbscripts/deploy/changelog2.sql
@@ -2,6 +2,6 @@
 
 -- changeset Chandramani:1
 copy into tesla_data
-from @tesla_db_github.TESLA_SCHEMA.tesla_stage
+from @tesla_stage
 file_format=( type =csv field_delimiter=',' skip_header = 1)
 files= ('TSLA.csv');
