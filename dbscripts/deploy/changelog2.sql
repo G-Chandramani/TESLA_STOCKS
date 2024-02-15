049fc1904227e91daadadcd7f7e62abdab8704cc
Chandramani G <gajbharechandramani@gmail.com>
2024-02-16 00:20:22 +0530

    Update changelog2.sql

--- dbscripts/deploy/changelog2.sql
+++ dbscripts/deploy/changelog2.sql
@@ -1,7 +1,11 @@
 -- liquibase formatted sql
 
 -- changeset Chandramani:1
+CREATE STAGE IF NOT EXISTS TESLA_DB_GITHUB.***.TESLA_STAGE
+URL='s3://your-s3-bucket/path/to/stage'
+STORAGE_INTEGRATION = s3_int;
+
 copy into tesla_data
-from @tesla_stage
+from @TESLA_DB_GITHUB.***.TESLA_STAGE
 file_format=( type =csv field_delimiter=',' skip_header = 1)
 files= ('TSLA.csv');
