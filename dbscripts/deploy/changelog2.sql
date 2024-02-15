a3f4f8641584fdf1bc4c63fcad3a38f70e0fb34f
Chandramani G <gajbharechandramani@gmail.com>
2024-02-16 00:10:11 +0530

    trying to load from aws schema

--- dbscripts/deploy/changelog2.sql
+++ dbscripts/deploy/changelog2.sql
@@ -1,14 +1,7 @@
 -- liquibase formatted sql
 
 -- changeset Chandramani:1
-use database tesla_db_github;
-
-use schema TESLA_SCHEMA;
-
-use warehouse CHANDRAMANI; 
-
-create or replace stage tesla_db_github.TESLA_SCHEMA.tesla_stage
-url ='s3://chintusnowflake/csv/project1/'
-STORAGE_INTEGRATION = s3_int;
-
-list @tesla_stage;
+copy into tesla_data
+from @tesla_db_github.TESLA_SCHEMA.tesla_stage
+file_format=( type =csv field_delimiter=',' skip_header = 1)
+files= ('TSLA.csv');
