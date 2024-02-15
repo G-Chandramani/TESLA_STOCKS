1c95bae6217beee811694f81b695e9657636d579
Chandramani G <gajbharechandramani@gmail.com>
2024-02-15 23:54:47 +0530

    Update changelog1.sql

--- dbscripts/deploy/changelog1.sql
+++ dbscripts/deploy/changelog1.sql
@@ -20,3 +20,13 @@ url ='s3://chintusnowflake/csv/project1/'
 STORAGE_INTEGRATION = s3_int;
 
 list @tesla_stage;
+
+--changeset Chandramani:2
+copy into tesla_data
+from @tesla_stage
+file_format=( type =csv field_delimiter=',' skip_header = 1)
+-- pattern = '.*TSLA.*';
+files= ('TSLA.csv');
+
+
+select * from tesla_data limit 100;
